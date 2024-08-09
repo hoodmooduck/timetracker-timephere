c3e5c3c94ffc1102c32562a68b017a0a04b4cb28
Denis T <goodeyxd@mail.ru>
2024-08-10 03:36:56 +0500

    <fix> - delete ui

--- src/Components/ProjectsContainer/ProjectsList/ProjectsList.tsx
+++ src/Components/ProjectsContainer/ProjectsList/ProjectsList.tsx
@@ -3,33 +3,24 @@ import ProjectCard from "../../../UI/ProjectCard/ProjectCard.tsx";
 import BigButton from "../../../UI/BigButton/BigButton.tsx";
 
 interface Props {
-    openModal: () => void,
-    projects: projectsTypes[]
+  openModal: () => void;
+  projects: projectsTypes[];
 }
 
-function ProjectsList({projects, openModal}: Props) {
+function ProjectsList({ projects, openModal }: Props) {
+  return (
+    <div className="projects-list">
+      <BigButton onClick={() => openModal()} />
 
-
-    return (
-        <div className="projects-list">
-            <BigButton onClick={() => openModal()}/>
-
-            <div className="projects-list__container">
-                <div className="projects-list__title">Ваши проекты: </div>
-                {
-                    projects && projects.map(el => (
-                        <ProjectCard
-                            key={el.id}
-                            id={el.id}
-                            name={el.name}
-                            tasks={el.tasks}
-                        />
-                    ))
-                }
-            </div>
-
-        </div>
-    )
+      <div className="projects-list__container">
+        <div className="projects-list__title">Ваши проекты: </div>
+        {projects &&
+          projects.map((el) => (
+            <ProjectCard key={el.id} id={el.id} name={el.name} />
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default ProjectsList;
