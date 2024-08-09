92fba856087bb0f8ed92db94519f51cbdd5660ca
Denis T <goodeyxd@mail.ru>
2024-08-10 04:43:29 +0500

    <fix> - fix security)) nomero 2

--- src/Modules/Firebase/config.ts
+++ src/Modules/Firebase/config.ts
@@ -9,14 +9,24 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDx6EryFebQeXJ372MzdVr_h6lwWIJMNvk",
+//   authDomain: "diplom-4cb3c.firebaseapp.com",
+//   projectId: "diplom-4cb3c",
+//   storageBucket: "diplom-4cb3c.appspot.com",
+//   messagingSenderId: "126749110453",
+//   appId: "1:126749110453:web:5c564650c5f5c4c6c5cefc",
+//   measurementId: "G-TB3EZD0M3D",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
