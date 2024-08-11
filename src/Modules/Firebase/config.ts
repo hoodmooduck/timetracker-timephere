ba05148a86f87303260ce6a2ff23cd5b7f1189a4
Denis T <goodeyxd@mail.ru>
2024-08-11 20:32:59 +0500

    <fix> - some fix ui and config

--- src/Modules/Firebase/config.ts
+++ src/Modules/Firebase/config.ts
@@ -18,16 +18,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDx6EryFebQeXJ372MzdVr_h6lwWIJMNvk",
-//   authDomain: "diplom-4cb3c.firebaseapp.com",
-//   projectId: "diplom-4cb3c",
-//   storageBucket: "diplom-4cb3c.appspot.com",
-//   messagingSenderId: "126749110453",
-//   appId: "1:126749110453:web:5c564650c5f5c4c6c5cefc",
-//   measurementId: "G-TB3EZD0M3D",
-// };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
